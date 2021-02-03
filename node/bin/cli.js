5c5a9127cfb121bd5b9a70e7ee96d7ada0078405
Aliaksandr Salauyeu <asalauyeu@hubspot.com>
2021-02-03 15:37:04 +0300

    add bin/cli

---
+++ node/bin/cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+
+const authCommand = require('../src/commands/auth');
+const getCommand = require('../src/commands/get');
+const createCommand = require('../src/commands/create');
+const updateCommand = require('../src/commands/update');
+const deleteCommand = require('../src/commands/delete');
+const propertiesCommand = require('../src/commands/properties');
+const { logger } = require('../src/helpers/logger');
+
+yargs
+    .usage('Sample app that interacts with Hubspot CRM objects')
+    .exitProcess(false)
+    .fail((msg, err, yargs) => {
+        if (msg === null) {
+            yargs.showHelp();
+            process.exit(0);
+        } else {
+            logger.error(msg);
+            process.exit(1);
+        }
+    })
+    .command(authCommand)
+    .command(getCommand)
+    .command(createCommand)
+    .command(updateCommand)
+    .command(deleteCommand)
+    .command(propertiesCommand)
+    .help()
+    .recommendCommands()
+    .demandCommand(1, '')
+    .strictCommands().argv;
