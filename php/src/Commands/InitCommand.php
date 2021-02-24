e5b89aa08afee78365f3fd86bfd5ab8461c475e3
ksvirkou <ksvirkou@hubspot.com>
2021-02-24 16:11:28 +0300

    cs fix

--- php/src/Commands/InitCommand.php
+++ php/src/Commands/InitCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Commands;
 
 use Symfony\Component\Console\Command\Command;
@@ -9,33 +10,34 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class InitCommand extends Command
 {
     protected static $defaultName = 'app:init';
-    
-    protected $envFileName = __DIR__ . '/../../.env';
+
+    protected $envFileName = __DIR__.'/../../.env';
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        
+
         if (!file_exists($this->envFileName)) {
-            copy($this->envFileName . '.template', $this->envFileName);
+            copy($this->envFileName.'.template', $this->envFileName);
         } elseif (!$io->confirm('The file ".env" already exists. Overwrite?')) {
             return Command::SUCCESS;
         }
-        
+
         $apiKey = $this->askForApiKey($io);
-        
+
         $content = preg_replace(
             '/^HUBSPOT_API_KEY=.*$/m',
-            'HUBSPOT_API_KEY=' . $apiKey,
+            'HUBSPOT_API_KEY='.$apiKey,
             file_get_contents($this->envFileName)
         );
-        
+
         file_put_contents($this->envFileName, $content);
-        
+
         $io->writeln('API key was put to ".env" successfully.');
+
         return Command::SUCCESS;
     }
-    
+
     protected function askForApiKey(SymfonyStyle $io): string
     {
         return $io->ask(
